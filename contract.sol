fa55f1a8a45557a5d5626925a7642a84c48b3665
dhairyshil24 <dhairyshil87@gmail.com>
2024-06-27 19:10:10 +0530

    added constructor function

--- contract.sol
+++ contract.sol
@@ -1,77 +1,88 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.26;
 
-contract ERC20Token {
-    string public name = "MyToken";
-    string public symbol = "MTK";
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
 
-    mapping(address => uint256) private balances;
-    mapping(address => mapping(address => uint256)) private allowances;
+contract ERC20{
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+      string public name;
+      string public symbol;
+      uint256 immutable public decimals;
+     uint256 public totalsupply;
 
-    constructor(uint256 initialSupply) {
-        totalSupply = initialSupply * 10 ** uint256(decimals);
-        balances[msg.sender] = totalSupply;
-        emit Transfer(address(0), msg.sender, totalSupply);
-    }
+      event Transfer(address indexed from, address indexed to, uint256 value);
+      event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    function balanceOf(address account) public view returns (uint256) {
-        return balances[account];
-    }
+      mapping (address => uint256) public balanceof;
 
-    function transfer(address recipient, uint256 amount) public returns (bool) {
-        require(recipient != address(0), "Transfer to the zero address");
-        require(balances[msg.sender] >= amount, "Transfer amount exceeds balance");
+      mapping (address => mapping(address => uint256)) public allowance;
 
-        balances[msg.sender] -= amount;
-        balances[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
+      constructor(string memory _name, string memory _symbol, uint256 _decimals){
 
-    function approve(address spender, uint256 amount) public returns (bool) {
-        require(spender != address(0), "Approve to the zero address");
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
 
-        allowances[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
+      }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
-        require(sender != address(0), "Transfer from the zero address");
-        require(recipient != address(0), "Transfer to the zero address");
-        require(balances[sender] >= amount, "Transfer amount exceeds balance");
-        require(allowances[sender][msg.sender] >= amount, "Transfer amount exceeds allowance");
+      function transfer(address to, uint256 value, address state) external returns(bool){
+           
+         _transfer(msg.sender, state, to, value);
 
-        balances[sender] -= amount;
-        balances[recipient] += amount;
-        allowances[sender][msg.sender] -= amount;
-        emit Transfer(sender, recipient, amount);
-        return true;
-    }
+         return true;
 
-    function allowance(address owner, address spender) public view returns (uint256) {
-        return allowances[owner][spender];
-    }
+      }
 
-    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
-        require(spender != address(0), "Increase allowance to the zero address");
 
-        allowances[msg.sender][spender] += addedValue;
-        emit Approval(msg.sender, spender, allowances[msg.sender][spender]);
-        return true;
-    }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
-        require(spender != address(0), "Decrease allowance to the zero address");
-        require(allowances[msg.sender][spender] >= subtractedValue, "Decreased allowance below zero");
 
-        allowances[msg.sender][spender] -= subtractedValue;
-        emit Approval(msg.sender, spender, allowances[msg.sender][spender]);
-        return true;
-    }
+        function transferfrom(address from, address to, uint256 value, address state) external returns(bool){
+
+         _transfer(from, to, state, value);
+
+         return true;
+
+        }
+
+        function givemetoken() external {
+         balanceof[msg.sender] += 1e18;
+
+
+        }
+
+
+        function approve(address spender, uint256 value) external returns(bool){
+                   
+          allowance[msg.sender][spender] += value;
+
+          emit Approval(msg.sender , spender, value);
+
+         return true;
+
+        }
+
+
+        function _transfer(address from, address state, address to, uint256 value) private returns(bool){
+          
+          require(balanceof[from] >= value, "ERC20: Insufficient funds");
+
+
+          balanceof[from] -= value;
+          balanceof[to] += value;
+          
+          onepercent(state ,value);
+          emit Transfer(from, to, value);
+          return true;
+
+        }
+
+        function onepercent(address state, uint256 value) private returns(bool){
+
+         balanceof[state] += value / 100;
+
+         return true;
+                  
+
+
+        }
+
 }
