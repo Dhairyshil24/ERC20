5829e2f4c78c119afe1a3db65979fd574d4365a2
dhairyshil24 <dhairyshil87@gmail.com>
2024-07-14 00:27:20 +0530

    added deposite and redeem functionality

--- contract.sol
+++ contract.sol
@@ -8,20 +8,22 @@ contract ERC20{
       string public symbol;
       uint256 immutable public decimals;
      uint256 public totalsupply;
-     address public owner;
+    address public owner;
 
       event Transfer(address indexed from, address indexed to, uint256 value);
       event Approval(address indexed owner, address indexed spender, uint256 value);
+      event Burn(uint value);
 
       mapping (address => uint256) public balanceof;
 
       mapping (address => mapping(address => uint256)) public allowance;
 
-      constructor(string memory _name, string memory _symbol, uint256 _decimals){
+      constructor(address _owner , string memory _name, string memory _symbol, uint256 _decimals){
 
         _name = name;
         _symbol = symbol;
         _decimals = decimals;
+        _owner = owner;
 
       }
 
@@ -34,36 +36,13 @@ contract ERC20{
       }
 
       function _mint(address to, uint256 value) private {
-              
-              balanceof[to] += value;
-
-              totalsupply += value;
-
-              emit Transfer(address(0), to , value);
-
+        balanceof[to] += value;
+        totalsupply += value;
       }
 
-
-      function mint(address to, uint256 value) external {
-
+      function mint(address to, uint256 value) external payable {
         require(msg.sender == owner);
         _mint(to, value);
-
-      }
-
-      function _burn(address from, uint256 value) private{
-            balanceof[from] -= value;
-            totalsupply -= value;
-
-            emit Transfer(from, address(0),value);
-
-
-      }
-
-
-      function burn(address from, uint256 value) external {
-        require(msg.sender == owner);
-        _burn(from,value);--
       }
 
 
@@ -84,7 +63,7 @@ contract ERC20{
         }
 
 
-        function approve(address spender, uint256 value) external returns(bool){
+        function approve(address spender, uint256 value) public returns(bool){
                    
           allowance[msg.sender][spender] += value;
 
@@ -118,5 +97,41 @@ contract ERC20{
 
 
         }
+
+        function deposit(uint value) external payable{
+
+         _mint(msg.sender,value);
+
+         
+          
+       }
+
+
+       function redeem(uint value, address contrct_add) external {
+
+        approve(contrct_add,value);
+                   
+      // require(allowance[msg.sender][contrct_add] >= value, "Allowance exceeded");
+        require(balanceof[msg.sender] >= value, "No money");
+        allowance[msg.sender][contrct_add] -= value;
+
+        _transfer(msg.sender, contrct_add, contrct_add, value);
+
+
+         _burn(value);
+         payable(msg.sender).transfer(value);
+
+                  
+
+       }
+
+       function _burn(uint value) private {
+
+        totalsupply -= value;
+
+        emit Burn(value);
+
+
+       }
 
 }
